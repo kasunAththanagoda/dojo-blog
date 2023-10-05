d8828715e5ab46c4f9c5bb898d5813ad99bec2a7
kasun aththanagoda <kasunaththanagoda@gmail.com>
2023-10-06 01:38:11 +0530

    31 delete blogs

--- jojo-blog/src/BlogDetails.js
+++ jojo-blog/src/BlogDetails.js
@@ -1,9 +1,20 @@
-import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { useHistory, useParams } from "react-router-dom/cjs/react-router-dom.min";
 import useFetch from "./customHooks/useFetch";
 
 const BlogDetails = () => {
     const {id}= useParams();
     const {data : blog,error,isPending}= useFetch("http://localhost:8000/blogs/"+id);
+    const history=useHistory();
+
+    const handleDelete=()=>{
+        fetch("http://localhost:8000/blogs/"+blog.id,{
+            method : "DELETE",
+        })
+        .then(()=>{
+            history.push("/");
+        })
+    };
+
     return ( 
         <div className="blog-details">
             {isPending && <div>loading....</div>}
@@ -13,6 +24,7 @@ const BlogDetails = () => {
                     <h2>{blog.title}</h2>
                     <p>written by : {blog.author}</p>
                     <div>{blog.body}</div>
+                    <button onClick={handleDelete}>Delete Blog</button>
                 </article>
             )}
             
