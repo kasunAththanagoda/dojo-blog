265261f0c639c400bfaef37146bbeaab970331f2
kasun aththanagoda <kasunaththanagoda@gmail.com>
2023-10-05 23:49:50 +0530

    19 handling fetch error

--- jojo-blog/src/Home.js
+++ jojo-blog/src/Home.js
@@ -5,6 +5,7 @@ const Home = () => {
     
 const [blogs,setBlogs]=useState();
 const [isPending,setIsPending]=useState(true);
+const [error,setError]=useState(null);
 
 
 
@@ -18,12 +19,23 @@ useEffect(()=>{
     console.log("use effects running")
     fetch("http://localhost:8000/blogs")
     .then(res=>{
+        console.log("res ",res)
+        if(!res.ok){
+            setIsPending(false);
+            throw Error("could not fetch the data..")
+        }
         return res.json();
     })
     .then(data=>{
         console.log("data :",data)
         setBlogs(data);
         setIsPending(false);
+        setError(null)
+    })
+    .catch(err=>{
+        console.log(err.message);
+        setIsPending(false);
+        setError(err.message)
     })
 },[]);
 
@@ -31,6 +43,7 @@ useEffect(()=>{
     return ( 
         <div className="Home">
             {isPending && <div>Loading....</div>}
+            {error && <div>{error}</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs" ></BlogList>}
             {/* <BlogList blogs={blogs.filter((blog)=>blog.author==="mario")} title="mario's Blogs"></BlogList> */}
            
