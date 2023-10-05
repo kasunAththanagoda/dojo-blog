5ea1f74fbaf920f7e713e5565c21e6d60993a360
kasun aththanagoda <kasunaththanagoda@gmail.com>
2023-10-06 01:19:20 +0530

    27 forms

--- jojo-blog/src/Create.js
+++ jojo-blog/src/Create.js
@@ -1,7 +1,30 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title,setTitle]=useState("");
+    const [author,setAuthor]=useState("mario");
+    const [body,setBody]=useState("");
+
     return ( 
         <div className="create">
             <h2>Add a New Blog</h2>
+            <form>
+                <label>Blog title : </label>
+                <input type="text" required value={title} onChange={(e)=>setTitle(e.target.value)}></input>
+
+                <label>Blog body : </label>
+                <textarea required value={body} onChange={(e)=>setBody(e.target.value)}></textarea>
+
+                <label>Blog author : </label>
+                <select value={author} onChange={(e)=>setAuthor(e.target.value)}>
+                    <option value="mario">mario</option>
+                    <option value="yoshi">yoshi</option>
+                </select>
+                <br></br>
+                <button>Add Blog</button>
+            </form>
+
+            {/* ---{title}--{body}--{author} */}
         </div>
 
      )
